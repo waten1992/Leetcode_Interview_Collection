7611c525516e02a5ef9af51d7814f56f5928c8fd
waten <waten1992@gmail.com>
2015-01-13 01:26:14 +0800

    Update memcpy.cpp
    Change format for memcpy.cpp

--- memcpy.cpp
+++ memcpy.cpp
@@ -5,18 +5,16 @@ using namespace std;
 void *mymemcpy(void *dest , const  void *src , size_t size )
 {
 	char *post_src ;
-    char *post_dest ;
-
+	char *post_dest ;
 	if (NULL == dest || NULL == src)
 		return NULL;
-	
-		post_src = (char *)src + size -1 ;
-		post_dest = (char *)dest + size -1 ;
-		while(size--)
-		{
-			*post_dest-- = *post_src--;
-		}
-	return dest ;
+	post_src = (char *)src + size -1 ;
+	post_dest = (char *)dest + size -1 ;
+	while(size--)
+	{
+		*post_dest-- = *post_src--;
+	}
+return dest ;
 }
 int main()
 {
