01a769761bbe9f1f5c49a945137fce87dd85af00
waten <waten1992@gmail.com>
2015-02-25 22:05:20 +0800

    层次遍历的另一种方案

--- Tree/T004_Level_Traversal.cpp
+++ Tree/T004_Level_Traversal.cpp
@@ -7,7 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- /*
+ /*-----------------------------------------
  
     用队列queue<TreeNode *> qu 存储每一层元素，核心是如何分离每一层。
 方案一：
@@ -16,7 +16,10 @@
     3-使用整层追加，分离代码，让代码的结构清晰简单；
 方案二：
     1-采用连个队列，Current_level--->存储本层元素 , Next_level-->存储下一层的元素；
- */
+ -------------------------------------------*/
+/*+++++++++++++++++++++++++
+ 方案一：Runtime: 10 ms    
++++++++++++++++++++++++++*/
 class Solution {
 public:
     vector<vector<int> > levelOrder(TreeNode *root)  //使用NULL 分离 ， 使得代码清晰易懂
@@ -52,5 +55,39 @@ public:
             Next_Element.push_back(Tmp->val) ; //存储到vector中
         }
     return Next_Element ;
+    }
+};
+
+/*--------------------------------------------------------------------------------------------*/
+/*==============
+Runtime: 8 ms
+===============*/
+class Solution {
+public:
+    vector<vector<int> > levelOrder(TreeNode *root)  //使用NULL 分离 ， 使得代码清晰易懂
+    {
+        queue<TreeNode *> Current_level ,  Next_level ;
+        vector<vector<int>> result ;
+        vector<int> level; //储存每一层的值
+        if(root == NULL)
+            return result;
+        Current_level.push(root);
+        while(!Current_level.empty()) //直到所有的元素都归为零
+        {
+             while(!Current_level.empty()) //把下一层的元素用Next_level 收集起来；
+             {
+                TreeNode * Tmp = Current_level.front();
+                Current_level.pop();
+                level.push_back(Tmp->val);//搜集每一层的值 
+                if(Tmp->left != NULL)
+                    Next_level.push(Tmp->left);
+                if(Tmp->right != NULL)
+                    Next_level.push(Tmp->right);
+             }
+            result.push_back(level);  //追加整层
+            level.clear();//清空level 的值
+            swap(Current_level,Next_level); //交换两层元素，使得Current_level 继续循环，直到Next_level 为空了
+        }
+     return result;  
     }
 };
