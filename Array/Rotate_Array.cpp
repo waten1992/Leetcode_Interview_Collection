e05a770d2428e8c9b08288b5ade9e18ecc5bf4c4
waten <waten1992@gmail.com>
2015-02-27 16:27:18 +0800

    leetcode AC

--- Array/Rotate_Array.cpp
+++ Array/Rotate_Array.cpp
@@ -24,7 +24,28 @@ public:
          }
     }
 };
----------------
+/*---------------
 为什么LEETCODE 总是说Last executed input: [1,2,3], 0
 不知道哪里出了问题了
 --------------
+*/
+/*-----------------------------------------
+直接调用C++自带的void reverse( BidirIt first, BidirIt last )
+Runtime: 27 ms
+*/  
+class Solution {
+public:
+    void rotate(int nums[], int n, int k) 
+    {   
+        k = k%n ;
+        if(k==0 || n==0)
+            return ;
+        else
+        {
+            reverse(nums, nums+n-k);//前半部分
+            reverse(nums+n-k ,nums+n);//后半部分
+            reverse(nums,nums+n);//整体旋转
+        }
+    }
+   
+};
